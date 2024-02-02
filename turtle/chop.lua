f1bf4b938f6f89df99ba505723d91489aed34a33
OvaOksehode <ovoksehode@gmail.com>
2024-02-02 01:28:32 +0100

    updated turtle.chop

--- turtle/chop.lua
+++ turtle/chop.lua
@@ -1,1 +1,21 @@
-print(turtle.inspect().name)
+local function chop()
+    local _,t = turtle.inspect()
+
+    local h = 0
+
+    if not(string.find(t.name, "log")) then print("not log") return end
+
+    while string.find(t.name, "log") do
+        turtle.dig()
+        turtle.digUp()
+        h = h + 1
+        turtle.up()
+        _,t = turtle.inspect()
+    end
+    
+    for i = 1, h do
+        turtle.down()
+    end
+end
+
+return {chop=chop}
